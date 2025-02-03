4b553683cd70265e2dcf8035202ffce84a014f5d
Brandon Johnson <johnsbr99@gmail.com>
2025-02-03 08:07:12 -0700

    Entry elements rendered dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import Header from './components/Header'
 import Entry from './components/Entry'
+import data from './data'
+
+console.log(data);
 
 export default function App() {
+
+  const entryElements = data.map((entry) => {
+    return (
+      <Entry 
+        img={entry.img }
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        title={entry.title}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    )
+  })
+
   return (
-    <div className="pageContainer">
+    <>
       <Header />
-      <Entry />
-    </div>
+      <main className="pageContainer">
+        {entryElements}
+      </main>
+    </>
   )
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,22 +1,32 @@
-export default function Entry() {
+export default function Entry(props) {
     return(
         <>
             <article className="entryContainer">
                 <div className="imageContainer">
-                    <img src="/mtFuji.png" alt="Picture of Mount Fuji in Japan." className="entryImage"/>
+                    <img 
+                        className="entryImage"
+                        src={props.img.src}
+                        alt={props.img.alt} 
+                    />
                 </div>
                 <aside className="entryDescription">
                     <div className="locationData">
-                        <img src="/marker.png" alt="Location marker icon" className="markerImage" />
-                        <span className="locationName">Japan</span>
-                        <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu&g_ep=EgoyMDI1MDEyOS4xIKXMDSoASAFQAw%3D%3D" target="_blank" className="mapsLink">View on Google Maps</a>
+                        <img
+                            className="markerImage" 
+                            src="/marker.png" 
+                            alt="Location marker icon" 
+                        />
+                        <span className="locationName">{props.country}</span>
+                        <a
+                            className="mapsLink"
+                            href={props.googleMapsLink} 
+                            target="_blank"
+                        >View on Google Maps</a>
                     </div>
-                    <h1 className="entryTitle">MountFuji</h1>
-                    <span className="entryDate">12 Jan, 2023 - 24 Jan, 2023</span>
+                    <h1 className="entryTitle">{props.title}</h1>
+                    <span className="entryDate">{props.dates}</span>
                     <p className="entryText">
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters 
-                    (12,380 feet). Mount Fuji is the single most popular tourist site in 
-                    Japan, for both Japanese and foreign tourists.
+                    {props.text}
                     </p>
                 </aside>
             </article>
